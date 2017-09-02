57558379846e9d4c0650090c2520d2c58247f528
Natasha Petrus <hello@natashapetrus.com>
2017-09-02 06:28:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ function add2(n) {
   const two = 2
 }
 
-var funkyFunction = function() {
-  return function() {
+var funkyFunction = function a() {
+  return function b() {
     return "FUNKY!"
   }
 }
